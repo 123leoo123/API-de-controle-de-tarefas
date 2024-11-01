05baa2c579b0d70a6e40df3905781c3ef9f1f26c
Leonardo Leao <leonardodleao@gmail.com>
2024-10-31 21:34:13 -0300

    Update Render

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import "reflect-metadata";
-import jwt from "jsonwebtoken";
 import "express-async-errors";
 import express, { json } from "express";
 import helmet from "helmet";
+import cors from "cors";
 import "dotenv/config";
 import { taskRouter } from "./route/task.route";
 import { categoryRoutes } from "./route/category.route";
@@ -12,6 +12,8 @@ import { userRouter } from "./route/user.route";
 export const app = express();
 
 console.log(process.env.EXAMPLE)
+
+app.use(cors());
 
 app.use(helmet());
 
